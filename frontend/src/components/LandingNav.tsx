fbe201e90a2e45833a45922c1e6d7a28e8a7b3aa
Somash Manandhar <manandharsomash@gmail.com>
2024-06-10 16:04:37 +0545

    implement design

--- frontend/src/components/LandingNav.tsx
+++ frontend/src/components/LandingNav.tsx
@@ -1,4 +1,5 @@
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Button from "./Button";
 export default function LandingNav() {
   return (
     <nav>
@@ -13,13 +14,15 @@ export default function LandingNav() {
             <NavLink to="/about">About Us</NavLink>
           </li>
           <li>
-            <NavLink  to="/teams">Teams</NavLink>
+            <NavLink to="/teams">Teams</NavLink>
           </li>
           <li>
-            <NavLink  to="/contact">Contact</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
           </li>
           <li>
-            <NavLink  to="/join">Get started</NavLink>
+            <NavLink to="/join">
+              <Button>Get started</Button>
+            </NavLink>
           </li>
         </div>
       </ul>
