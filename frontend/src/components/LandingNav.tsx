76751803c2c44ec7bac36b9e9ae124ca7e703b19
suminashrestha <suminastha77@gmail.com>
2024-06-12 00:08:42 +0545

    Logo

--- frontend/src/components/LandingNav.tsx
+++ frontend/src/components/LandingNav.tsx
@@ -6,7 +6,9 @@ export default function LandingNav() {
       <ul className="flex justify-between h-[10%] items-center p-5 w-[100%]">
         <div className="w-[20%] flex items-center justify-center">
           <li className="transition ease-in-out delay-60 hover:-translate-y-1 hover:scale-110 hover:text-btnColor duration-300">
-            <NavLink to="/">Sahaara</NavLink>
+            <NavLink to="/">
+              <img className="h-[90%] w-[50%]" src="/Logo.png" alt="Sahaara" />
+            </NavLink>
           </li>
         </div>
 
