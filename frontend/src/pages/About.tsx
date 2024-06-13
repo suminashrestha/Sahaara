8294bdde8da395a551422069275893909c71e7dc
suminashrestha <suminastha77@gmail.com>
2024-06-14 00:14:50 +0545

    About page ui

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,7 +3,9 @@ import SliderComponent from "../components/Slider";
 const About = () => {
   return (
     <div>
-      <h1 className="text-secondary text-center text-3xl p-2 m-2">About Us</h1>
+      <h1 className="text-secondary text-center font-bold font-Roboto text-7xl p-2 m-2">
+        About Us
+      </h1>
       <div style={{ position: "relative" }}>
         <SliderComponent />
       </div>
