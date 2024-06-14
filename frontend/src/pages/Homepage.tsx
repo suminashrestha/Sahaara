47d350131e0f427ed8a2d6115d4d5fd2fd8e082d
suminashrestha <suminastha77@gmail.com>
2024-06-14 23:31:00 +0545

    Homepage done

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,15 +1,32 @@
 export default function Homepage() {
   return (
     <>
-      <div className="flex flex-col max-h-screen overflow-y-scroll">
-        <div className="w-full bg-[#d6a9d5] text-white p-5 text-center text-2xl">
-          Welcome
-        </div>
-
-        <div className=" flex-auto relative w-2/3 h-120 bg-cover bg-center bg-np-repeat bg-[url('/playDog.jpeg')]">
-          <div className="absolute bottom-0 right-0 bg-gray-2000 bg-opacity-50 text-white text-2xl p-6 m-4 w-1/3 h-1/3 backdrop-blur-md">
-            Help Save Lives
+      <div className="w-full h-full flex  ">
+        <div className="flex-auto items-center w-[50%] h-[80%]  m-4 flex-cols gap-2">
+          <div className="flex flex-col items-center w-full h-[65%] m-2  justify-around">
+            <h1 className="text-black font-semibold font-Oswald tracking-tighter text-7xl m-11 p-2 ">
+              Connecting hearts and homes for every animal in need
+            </h1>
+            <p className=" p-2 text-3xl m-11">
+              Join us in making a difference in the lives of animals – together,
+              we can create happier, healthier futures for every pet in need.
+            </p>
           </div>
+          <div className="flex items-center w-[90%] h-[30%]  m-9 p-12 flex-cols gap-9">
+            <button className="bg-btnColor px-12 py-6 text-xl font-bold rounded-lg text-white hover:bg-btnHover">
+              Donate Now
+            </button>
+            <button className="bg-btnColor px-12 py-6 text-xl font-bold rounded-lg text-white hover:bg-btnHover">
+              Join Us
+            </button>
+          </div>
+        </div>
+        <div className="flex-auto items-center w-[50%] h-[80%] bg-blue-200 m-4 flex-cols gap-5">
+          <img
+            className="w-full h-full object-cover"
+            src="sadFace.jpeg"
+            alt="dog"
+          />
         </div>
       </div>
     </>
