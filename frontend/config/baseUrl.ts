7393f6e93a064a9c3532885a831bc503916907ac
ShirshakShahi <royalshirshal000@gmail.com>
2024-07-15 22:20:20 +0545

    add: interceptors

--- frontend/config/baseUrl.ts
+++ frontend/config/baseUrl.ts
@@ -1,24 +1,20 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
-// import {
-//   TOKEN_LOCAL_STORAGE,
-//   getLocalStorage,
-// } from "";
 
 const API: AxiosInstance = axios.create({
   baseURL: "http://localhost:8080",
 });
-// API.post("/api/v1/user",{})
-// API.interceptors.request.use(
-//   (config: InternalAxiosRequestConfig<any>) => {
-//     const token = getLocalStorage(TOKEN_LOCAL_STORAGE);
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error: any) => {
-//     return Promise.reject(error);
-//   }
-// );
+
+API.interceptors.request.use(
+  (config: InternalAxiosRequestConfig<any>) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
